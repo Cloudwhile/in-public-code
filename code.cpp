8f2145bda4e23e4286644cfdc2e75adaf0f4076b
Cloudwhile <86512509+Cloudwhile@users.noreply.github.com>
2021-07-03 21:14:49 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,7 +2,16 @@
 #include<string.h>
 #include<windows.h>
 void ain(float a, b){
-
+float c;
+char nu;
+cout<<"Calculate(a?b[1+1]):";
+cin>>a>>nu>>b;
+switch (nu){
+case '+':
+    c = a + b;
+    cout<<a<<nu<<b<<"="<<c;
+    break;
+}
 }
 int main (){
 
