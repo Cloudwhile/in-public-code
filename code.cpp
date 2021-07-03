32d8d58b7ae0d9e5ba8d2161f848fcf8edbe7699
Cloudwhile <86512509+Cloudwhile@users.noreply.github.com>
2021-07-03 21:24:39 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -6,11 +6,31 @@ float c;
 char nu;
 cout<<"Calculate(a?b[1+1]):";
 cin>>a>>nu>>b;
+while(1){
 switch (nu){
 case '+':
     c = a + b;
     cout<<a<<nu<<b<<"="<<c;
     break;
+case '-':
+    c = a - b;
+    cout<<a<<nu<<b<<"="<<c;
+    break;
+case '*':
+    c = a * b;
+    cout<<a<<nu<<b<<"="<<c;
+    break;
+case '/':
+    if b == 0{
+    cout<<"the second number must not be zero!"
+    break;
+}
+else {
+    c = a / b
+    cout<<a<<nu<<b<<"="<<c;
+    break;
+}
+}
 }
 }
 int main (){
